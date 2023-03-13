84723acb54020518e852f6e9e8ba834af66e98de
Gabrielle Schryver <gabbyschryver@gmail.com>
2023-03-13 11:26:03 -0700

    completed exercises for all of book 4

---
+++ scripts/Metals.js
@@ -0,0 +1,35 @@
+import { getMetals, setMetal } from "./database.js"
+
+const metals = getMetals()
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "metal") {
+            window.alert(`User chose metal ${event.target.value}`)
+        }
+    }
+)
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "metal") {
+            setMetal(parseInt(event.target.value))
+        }
+    }
+)
+
+export const Metals = () => {
+    let html = "<ul>"
+
+    // This is how you have been converting objects to <li> elements
+    for (const metal of metals) {
+        html += `<li id="metal--${metal.id}">
+            <input type="radio" name="metal" value="${metal.id}" /> ${metal.metal}
+        </li>`
+    }
+
+    html += "</ul>"
+    return html
+}
